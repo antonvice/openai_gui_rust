4735537e8c9350c284457eed495611e4facfda02
Anton <a1996nton@gmail.com>
2023-07-30 18:00:06 +0700

    v_1.0

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,118 @@
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Box, Button, Entry, Orientation, Stack, StackSwitcher};
+use std::env;
+
 mod q_logic;
 
-use gtk::prelude::*;
-use gtk::{glib, Application, ApplicationWindow, Button, Entry};
-use std::env;
 const APP_ID: &str = "org.gtk_rs.GPT4CHAT";
 
-fn main() -> glib::ExitCode {
-    // Create a new application
+fn main() {
     let app = Application::builder().application_id(APP_ID).build();
 
-    // Connect to "activate" signal of `app`
-    app.connect_activate(build_ui);
+    app.connect_activate(|app| {
+        let token_entry = Entry::builder()
+            .margin_top(12)
+            .margin_bottom(12)
+            .margin_start(12)
+            .margin_end(12)
+            .placeholder_text("Enter your API token here...")
+            .build();
 
-    // Run the application
-    app.run()
-}
+        let token_button = Button::builder()
+            .label("Save token and start chat")
+            .margin_top(12)
+            .margin_bottom(12)
+            .margin_start(12)
+            .margin_end(12)
+            .build();
 
-fn build_ui(app: &Application) {
-    // Create an Entry (text field) to get user input
-    let entry = Entry::builder()
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .placeholder_text("Enter text here...")
-        .build();
+        let token_box = Box::new(Orientation::Vertical, 6);
+        token_box.append(&token_entry);
+        token_box.append(&token_button);
 
-    // Create a button with label and margins
-    let button = Button::builder()
-        .label("Press me!")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
+        let chat_entry = Entry::builder()
+            .margin_top(12)
+            .margin_bottom(12)
+            .margin_start(12)
+            .margin_end(12)
+            .placeholder_text("Enter your message here...")
+            .build();
 
-    let reveal_button = Button::builder()
-        .label("PYour code will display here!")
-        .margin_top(12)
-        .margin_bottom(12)
-        .margin_start(12)
-        .margin_end(12)
-        .build();
+        let chat_button = Button::builder()
+            .label("Send message")
+            .margin_top(12)
+            .margin_bottom(12)
+            .margin_start(12)
+            .margin_end(12)
+            .build();
 
-    // Create a vertical box to hold the entry and the button
-    let vbox = gtk::Box::new(gtk::Orientation::Vertical, 6);
+        let user_message_view = gtk::TextView::builder()
+            .editable(false)
+            .margin_bottom(20)
+            .margin_start(20)
+            .margin_end(20)
+            .margin_top(20)
+            .build();
+        
+        let assistant_message_view = gtk::TextView::builder()
+            .editable(false)
+            .margin_bottom(20)
+            .margin_start(20)
+            .margin_end(20)
+            .margin_top(20)
+            .build();
+        
+        let chat_box = Box::new(Orientation::Vertical, 6);
+        chat_box.append(&chat_entry);
+        chat_box.append(&chat_button);
+        chat_box.append(&user_message_view);
+        chat_box.append(&assistant_message_view);
 
-    // Pack the entry and the button into the box
-    vbox.append(&entry);
-    vbox.append(&button);
-    vbox.append(&reveal_button);
+        let chat_entry_clone = chat_entry.clone();
+        let user_message_view_clone = user_message_view.clone();
+        let ass_view_clone = assistant_message_view.clone();
 
-    // Create a window
-    let window = ApplicationWindow::builder()
-        .application(app)
-        .title("My GTK App")
-        .child(&vbox)
-        .build();
+        chat_button.connect_clicked(move |_| {
+            // Get the text from the chat_entry
+            let user_message = chat_entry_clone.text().to_string();
+        
+            let assistant_response: String = q_logic::respond(&user_message);
 
-    // Connect to "clicked" signal of `button`
-    button.connect_clicked(move |button| {   
-        // Get the text from the entry
-        let token = entry.text().to_string();
-        env::set_var("OPENAI_API_KEY", &token);
-        // Set the label to "Saved!" after the button has been clicked on
-        button.set_label("API KEY Saved");
-        reveal_button.set_label(&format!("Your code: {}", entry.text()));
-        // Use the `token` variable globally or for further processing
-        println!("User input: {}", token);
+            // Display the user message
+            let buffer = user_message_view_clone.buffer();
+            buffer.set_text(&user_message);
+            let a_buffer = ass_view_clone.buffer();
+            a_buffer.set_text(&assistant_response);
+            // Clear the chat_entry
+            chat_entry_clone.set_text("");
+        });
+
+        let stack = Stack::new();
+        stack.add_titled(&token_box, Some("token"), "API Token");
+        stack.add_titled(&chat_box, Some("chat"), "Chat");
+
+        let stack_switcher = StackSwitcher::builder().stack(&stack).build();
+
+        let vbox = Box::new(Orientation::Vertical, 6);
+        vbox.append(&stack_switcher);
+        vbox.append(&stack);
+
+        let token_entry_clone = token_entry.clone();
+        let stack_clone = stack.clone();
+        token_button.connect_clicked(move |_| {
+            let token = token_entry_clone.text().to_string();
+            env::set_var("OPENAI_API_KEY", &token);
+            stack_clone.set_visible_child_name("chat");
+        });
+
+        let window = ApplicationWindow::builder()
+            .application(app)
+            .title("Chat App")
+            .child(&vbox)
+            .build();
+
+        window.present();
     });
 
-    // Present window
-    window.present();
+    app.run();
 }
--- src/q_logic.rs
+++ src/q_logic.rs
@@ -1,14 +1,14 @@
 use openai_api_rust::*;
 use openai_api_rust::chat::*;
-use openai_api_rust::completions::*;
+// use openai_api_rust::completions::*;
 
-fn main() {
+pub fn respond(text: &str) -> String {
     // Load API key from environment OPENAI_API_KEY.
     // You can also hadcode through `Auth::new(<your_api_key>)`, but it is not recommended.
     let auth = Auth::from_env().unwrap();
     let openai = OpenAI::new(auth, "https://api.openai.com/v1/");
     let body = ChatBody {
-        model: "gpt-3.5-turbo".to_string(),
+        model: "gpt-4".to_string(),
         max_tokens: Some(7),
         temperature: Some(0_f32),
         top_p: Some(0_f32),
@@ -19,10 +19,11 @@ fn main() {
         frequency_penalty: None,
         logit_bias: None,
         user: None,
-        messages: vec![Message { role: Role::User, content: "Hello!".to_string() }],
+        messages: vec![Message { role: Role::User, content: text.to_string() }],
     };
     let rs = openai.chat_completion_create(&body);
     let choice = rs.unwrap().choices;
     let message = &choice[0].message.as_ref().unwrap();
-    assert!(message.content.contains("Hello"));
+    println!("{}", message.content);
+    return message.content.clone();
 }
